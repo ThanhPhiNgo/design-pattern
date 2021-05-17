b15d1012725d8ab6c526dbab32a1ae84dead4b12
Thanh Phi <thanhphi.dd@gmail.com>
2021-05-17 16:07:28 +0700

    add adapter pattern example into project

---
+++ design-pattern/src/main/java/com/phint2/pattern/structural/adapter/EnglishAdaptee.java
@@ -0,0 +1,10 @@
+package com.phint2.pattern.structural.adapter;
+
+public class EnglishAdaptee {
+
+	public void receive(String words) {
+
+		System.out.println("Retrieving words from Adapter...");
+		System.out.println(words);
+	}
+}
