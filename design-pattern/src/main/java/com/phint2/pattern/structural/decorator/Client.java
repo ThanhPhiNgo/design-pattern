4ca753ee2148bef42972bbb3cd7b175395265b33
Thanh Phi <thanhphi.dd@gmail.com>
2021-05-17 16:39:48 +0700

    add decorator pattern example into project

---
+++ design-pattern/src/main/java/com/phint2/pattern/structural/decorator/Client.java
@@ -0,0 +1,27 @@
+package com.phint2.pattern.structural.decorator;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        System.out.println("NORMAL EMPLOYEE");
+        EmployeeComponent employee = new EmployeeConcreteComponent("PHINT2");
+        employee.showBasicInformation();
+        employee.doTask();
+
+        System.out.println("\nTEAM LEADER: ");
+        EmployeeComponent teamLeader = new TeamLeader(employee);
+        teamLeader.showBasicInformation();
+        teamLeader.doTask();
+
+        System.out.println("\nMANAGER: ");
+        EmployeeComponent manager = new Manager(employee);
+        manager.showBasicInformation();
+        manager.doTask();
+
+        System.out.println("\nTEAM LEADER AND MANAGER: ");
+        EmployeeComponent teamLeaderAndManager = new Manager(teamLeader);
+        teamLeaderAndManager.showBasicInformation();
+        teamLeaderAndManager.doTask();
+    }
+}
