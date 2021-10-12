454d5d2510dc37a915acb80da0e215c4b352aca8
Thanh Phi <thanhphi.dd@gmail.com>
2021-10-12 22:54:28 +0700

    add facade pattern example into project

---
+++ design-pattern/src/main/java/com/phint2/pattern/structural/facade/AccountService.java
@@ -0,0 +1,8 @@
+package com.phint2.pattern.structural.facade;
+
+public class AccountService {
+
+	public void getAccount(String email) {
+		System.out.println("Getting the account of " + email);
+	}
+}
